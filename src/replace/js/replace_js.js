86aa2d8fb3ea7e71610b567f836c06dfb263d44e
Wladek Figura <Wladek.Figura@mottmac.com>
2024-07-19 16:07:17 +0100

    lambda replace

--- src/replace/js/replace_js.js
+++ src/replace/js/replace_js.js
@@ -4,3 +4,11 @@ const test = str => str.replace(/\.|trex|raptor/gi, '');
 console.log(test('.lolo.molo.')) // => lolomolo
 console.log(test('.lolotrex.molo.trex')) // => lolomolo
 console.log(test('.lolotrex.moloraptor.trex')) // => lolomolo
+
+const lambda = str => str.replace(/^./, x => x.toUpperCase())
+
+const lambda_2 = str => str.replace(/0|1|2|3|4/g, x => 'aeiou'[x])
+
+console.log(lambda('romek')) // => Romek
+
+console.log(lambda_2('01234')) // => aeiou
