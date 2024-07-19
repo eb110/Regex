380444e49bee759fc46d0de187ccc9468d5d8ef9
Wladek Figura <Wladek.Figura@mottmac.com>
2024-07-19 14:43:41 +0100

    all occurrence js

---
+++ src/all_occurrence/js/all_occurrence_js.js
@@ -0,0 +1,8 @@
+// /g => all accurrence
+
+const test = str => str.replace(/\.|trex|raptor/gi, '');
+
+
+console.log(test('.lolo.molo.')) // => lolomolo
+console.log(test('.lolotrex.molo.trex')) // => lolomolo
+console.log(test('.lolotrex.moloraptor.trex')) // => lolomolo
