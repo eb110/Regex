16be11eba07bc228454423e78cb872425c7416b9
Wladek Figura <Wladek.Figura@mottmac.com>
2024-07-19 14:18:12 +0100

    dev leanup

--- src/or/JS/or_js.js
+++
@@ -1,6 +0,0 @@
-const test = str => str.replace(/\.|trex|raptor/gi, '');
-
-
-console.log(test('.lolo.molo.')) // => lolomolo
-console.log(test('.lolotrex.molo.trex')) // => lolomolo
-console.log(test('.lolotrex.moloraptor.trex')) // => lolomolo
