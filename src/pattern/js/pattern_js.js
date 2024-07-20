0f325ac13782b5df0b89f179e74c7751690c25ca
eb110 <wfigura@op.pl>
2024-07-20 14:07:28 +0100

    bf_2

--- src/pattern/js/pattern_js.js
+++ src/pattern/js/pattern_js.js
@@ -1,9 +1,44 @@
+// => (.)\1 gives one or if exist -> two eles of same group
+// a or aa
+
 const brf = str => {
-    let pattern = /(.)\1{4}/;
-    let res = pattern.test(str);
-    return res;
+    let fivePattern = /(.)\1{4}/;
+    let fourPattern = /(.)\1{3}/;
+    let fullPattern = /(.)\1{2}(.)\2|(.)\3(.)\4{2}/;
+    let threePattern = /(.)\1{2}/;
+    let twoPattern = /(.)\1.\1|(.)\2(.)\3/;
+    if(fivePattern.test(str)) {
+        console.log(str + ' gives five pattern');
+        return;
+    }
+    if(fourPattern.test(str)) {
+        console.log(str + ' gives four pattern');
+        return;
+    }
+    if(fullPattern.test(str)) {
+        console.log(str + ' gives full pattern');
+        return;
+    }
+    if(threePattern.test(str)) {
+        console.log(str + ' gives three pattern');
+        return;
+    }
+    if(twoPattern.test(str)) {
+        console.log(str + ' gives two pattern');
+        return;
+    }
+    console.log(str + ' no pattern')
 }
 
 
-console.log(brf('!!!!!')); // => true
-console.log(brf('!!!!?')); // => false
+brf('!!!!!')
+brf('!!!!?')
+brf('!!!?!')
+brf('?!!!!')
+brf('!!!??')
+brf('!!???')
+brf('!???!')
+brf('!!?!!')
+brf('??!!?')
+brf('!??!!')
+brf('!?!!!')
